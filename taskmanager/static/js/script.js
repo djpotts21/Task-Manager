c7914ab7afdf3c074975c44a687b51fed01267c9
Daniel Potts. <djpotts2891@gmail.com>
2023-09-15 22:50:51 +0000

    Added css, js. Header, Footer and Tasks html base

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidebar initialization
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav);
+  });
