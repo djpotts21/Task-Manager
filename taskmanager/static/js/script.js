2c8b81d0dd1053986b66975573d97ffa100e8386
Daniel Potts. <djpotts2891@gmail.com>
2023-09-16 21:23:22 +0000

    Added delete function with defence modal

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidebar initialization
     var sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
+    var modal = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(modal);
   });
