ece01da2c3665f15cd57c1cbb301938e02bf3c54
Daniel Potts. <djpotts2891@gmail.com>
2023-09-17 20:58:49 +0000

    Add tasks, Display Tasks and asc functions

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function() {
     // sidebar initialization
-    var sidenav = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(sidenav);
-    var modal = document.querySelectorAll('.modal');
-    var instances = M.Modal.init(modal);
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+    //Collaspible initialization
+    let collaspible = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collaspible);
+    // Form initialization
+      // Modal initialization
+      let modal = document.querySelectorAll('.modal');
+      M.Modal.init(modal);
+      // Datepicker initialization
+      let datep = document.querySelectorAll('.datepicker');
+      M.Datepicker.init(datep, {
+        format: "dd mmmm, yyyy",
+        i18n: {done: "Select"}
+      });
+      // Select Box initialization
+      let selectdrop = document.querySelectorAll('select');
+      M.FormSelect.init(selectdrop);
   });
